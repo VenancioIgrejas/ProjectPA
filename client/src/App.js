8faf8f3bcbcf7fbcc99938ce1e9b6f8cd865db01
Kaylani <kaylani@gta.ufrj.br>
2021-05-15 16:21:38 -0300

    Add AddProductButton

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import './App.css';
 import { Box, ChakraProvider } from "@chakra-ui/react"
 import ProductsList from "./components/ProductsList";
+import AddProductButton from "./components/AddProductButton";
 import MainScreen from "./screens/MainScreen";
 
 function App() {
@@ -12,30 +13,29 @@ function App() {
     setTeste(teste + 1);
     console.log(teste);
   }
+
   return (
     <ChakraProvider>
       <div className="App">
-        <MainScreen />
-        <ProductsList />
+        <AddProductButton />
+
         <div>
           counter: {counter}
         </div>
-        <div>
-          teste: {teste}
-        </div>
+
+
         <button onClick={() => {
           counter++
           console.log(counter);
         }}>
           aumentar counter!
-    </button>
-        <button onClick={myFunc}>
-          aumentar teste!
-    </button>
+        </button>
+
+      <button onClick={myFunc}>
+        aumentar teste!
+      </button>
+
       </div>
-      <Box bg="tomato" w="100%" p={4} color='white'>
-        hello hello
-    </Box>
     </ChakraProvider>
   );
 }
