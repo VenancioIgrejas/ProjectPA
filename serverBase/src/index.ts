7a199e81985482e93522f7c28324eb75aa6ab6c6
VenancioIgrejas <venancio.igrejas@poli.ufrj.br>
2021-05-16 21:54:57 -0300

    ajuste da pasta

--- serverBase/src/index.ts
+++ serverBase/src/index.ts
@@ -11,10 +11,6 @@ import {Product} from "./entity/Product";
 import {Provider} from "./entity/Provider";
 
 
-import * as providerjson from './../../dataMock/MOCK_DATA_provider.json';
-import  * as categoryjson from './../../dataMock/MOCK_DATA_category.json';
-import  * as  usersjson from './../../dataMock/MOCK_DATA_users.json';
-import  * as  productjson from './../../dataMock/MOCK_DATA_product.json';
 
 createConnection().then(async connection => {
 
@@ -40,21 +36,6 @@ createConnection().then(async connection => {
 
     // start express server
     app.listen(3001);
-
-    // insert new users for test
-    // await connection.manager.save(connection.manager.create(User, {
-    //     login: "Timber",
-    //     password: "Saw"
-    // }));
-
-    // insert new users for test
-    // await connection.manager.save(connection.manager.create(User, usersjson));
-    // await connection.manager.save(connection.manager.create(Category, categoryjson));
-    // await connection.manager.save(connection.manager.create(Provider, providerjson));
-    // await connection.manager.save(connection.manager.create(Product, productjson));
-    // await connection.manager.save(connection.manager.createQueryBuilder().insert().into(User).values(usersjson).execute());
-    
-    // await connection.createQueryBuilder().insert().into(User).values(usersjson).execute();
 
     console.log("Express server has started on port 3001. Open http://localhost:3001/users to see results");
 
